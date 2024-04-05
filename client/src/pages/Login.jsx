ac08e816a9a28672b0bf8e4285b1f4ad84c0f9c5
unknown <rachael.kate.stokes@gmail.com>
2024-04-04 20:53:44 -0400

    fixed the install for tailwind with Anthony's help, added styling to the login page, created login form with card

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -49,34 +49,46 @@ const Login = () => {
       )
     } 
     return (
-      <form onSubmit={handleFormSubmit}>
+      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleFormSubmit}>
+        <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email</label>
         <input
-          placeholder="Your email"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Email"
           name="email"
           type="email"
           value={formState.email}
           onChange={handleChange}
         />
+        </div>
+        <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
         <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           placeholder="******"
           name="password"
           type="password"
           value={formState.password}
           onChange={handleChange}
         />
-        <button type="submit">
-          Submit
+        </div>
+        <div class= "flex items-center justify-between">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          Login
         </button>
+        </div>
       </form>
     );
   };
 
   return (
     <main>
+      <div class="w-full max-w-xs">
       <h4>Login</h4>
       <div>
         {renderForm()}
         {error && <div>{error.message}</div>}
+      </div>
       </div>
     </main>
   );
