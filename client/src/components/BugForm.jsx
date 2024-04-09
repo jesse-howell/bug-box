eb2e36186f5a2199746ad2fa02db08f3cb6f3990
sailorshy94 <sbutle94@gmail.com>
2024-04-08 22:01:13 -0400

    added logic to code to set error message for if bug input is left blank

--- client/src/components/BugForm.jsx
+++ client/src/components/BugForm.jsx
@@ -11,8 +11,17 @@ const BugForm = ({ userId }) => {
 
   const [addBug, { error }] = useMutation(ADD_BUG);
 
+  // handle Form errors
+  const [errorMessage, setErrorMessage] = useState('');
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+
+    // error msg for if bug input area is left blank
+    if(!bug) {
+      setErrorMessage('This field cannot be left blank, please enter a bug name...');
+      return;
+    }
 
     try {
       const data = await addBug({
@@ -48,9 +57,9 @@ const BugForm = ({ userId }) => {
               Add Bug
             </button>
           </div>
-          {error && (
+          {errorMessage && (
             <div className="col-12 my-3 bg-danger text-white p-3">
-              {error.message}
+              {errorMessage}
             </div>
           )}
         </form>
