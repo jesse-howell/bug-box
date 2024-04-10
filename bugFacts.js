9130f8b7c82eed62eb656da6998c029416829fb8
sailorshy94 <sbutle94@gmail.com>
2024-04-09 22:20:50 -0400

    added Pico comment to README, created a bugFacts file

---
+++ bugFacts.js
@@ -0,0 +1,27 @@
+const bugFacts = [
+    { 
+        id: 1,
+        fact: "While gathering food, a bee may fly up to 60 miles in one day.",
+        source: "https://www.si.edu/spotlight/buginfo/fun-facts-bugs",
+    },
+    {
+        id: 2,
+        fact: "Wasps feeding on fermenting juice have been known to get 'drunk' and pass out.",
+        source: "https://www.si.edu/spotlight/buginfo/fun-facts-bugs",
+    },
+    {
+        id: 3,
+        fact: "nsects have been present for about 350 million years, and humans for only 300,000 years.",
+        source: "https://www.si.edu/spotlight/buginfo/fun-facts-bugs",
+    },
+    {
+        id: 4,
+        fact: "True flies have only one pair of wings, and sometimes, none at all. A hind pair of 'wings' is reduced to balancing organs called halteres.",
+        source: "https://www.si.edu/spotlight/buginfo/fun-facts-bugs",
+    },
+    {
+        id: 5,
+        fact: "There are nearly as many species of ants (8,800) as there are species of birds (9,000) in the world.",
+        source: "https://www.si.edu/spotlight/buginfo/fun-facts-bugs",
+    }
+];
