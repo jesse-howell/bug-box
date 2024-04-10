af1bb1d9d57b85ac6a98a3e247277725fc53214a
sailorshy94 <sbutle94@gmail.com>
2024-04-09 22:23:37 -0400

    removed commented-out code from Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,27 +17,6 @@ const Header = () => {
         <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
           paragraph
         </p>
-        {/* <div>
-          {Auth.loggedIn() ? (
-            <>
-              <Link className="btn btn-lg btn-primary m-2" to="/me">
-                View My Profile
-              </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link className="btn btn-lg btn-primary m-2" to="/login">
-                Login
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
-                Signup
-              </Link>
-            </>
-          )} */}
-        {/* </div> */}
       </div>
     </header>
   );
